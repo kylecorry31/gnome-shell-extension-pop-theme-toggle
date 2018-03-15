9d3b425791e8eebefd8f9077f52c43cc9636061e
Kyle Corry <kylecorry31@gmail.com>
2018-03-15 19:34:45 -0400

    Add theme sub menu

--- extension.js
+++ extension.js
@@ -2,38 +2,45 @@ const Main = imports.ui.main;
 const PopupMenu = imports.ui.popupMenu;
 const Ornament = imports.ui.popupMenu.Ornament;
 const Util = imports.misc.util;
+const St = imports.gi.St;
 
 function init() {}
 
 function enable() {
+    this.mainMenu = Main.panel.statusArea['aggregateMenu'].menu;
+
+    this.themeMenu = new PopupMenu.PopupSubMenuMenuItem("Theme", true);
+    this.mainMenu.addMenuItem(themeMenu, 8);
+    this.themeMenu.icon.icon_name = "starred-symbolic";
     
-    this.powerMenu = Main.panel.statusArea['aggregateMenu']._power._item.menu;
-    
-    this.separator = new PopupMenu.PopupSeparatorMenuItem();
-    this.powerMenu.addMenuItem(this.separator, 0);
-    
-    this.light = new PopupMenu.PopupMenuItem("Light Theme");
+    this.light = new PopupMenu.PopupMenuItem("Light");
     this.light.connect('activate', (item, event) => {
         this.reset_ornament();
         this.set_theme("light");
         item.setOrnament(Ornament.DOT);
     });
-    this.powerMenu.addMenuItem(this.light, 0);
+    this.themeMenu.menu.addMenuItem(this.light, 0);
     
-    this.dark = new PopupMenu.PopupMenuItem("Dark Theme");
+    this.dark = new PopupMenu.PopupMenuItem("Dark");
     this.dark.connect('activate', (item, event) => {
         this.reset_ornament();
         this.set_theme("dark");
         item.setOrnament(Ornament.DOT);
     });
-    this.powerMenu.addMenuItem(this.dark, 0);
+    this.themeMenu.menu.addMenuItem(this.dark, 1);
     
     this.reset_ornament();
     this.light.setOrnament(Ornament.DOT);
+    this.set_theme("light");
 }
 
 function set_theme(theme) {
     Util.trySpawn(["pop-theme-toggle", theme]);
+    if(theme == "light") {
+        this.themeMenu.label.text = "Light Theme";
+    } else if (theme == "dark") {
+        this.themeMenu.label.text = "Dark Theme";
+    }
 }
 
 function reset_ornament() {
@@ -52,8 +59,8 @@ function disable() {
         this.dark = 0;
     }
 
-    if (this.separator) {
-        this.separator.destroy();
-        this.separator = 0;
+    if (this.themeMenu) {
+        this.themeMenu.destroy();
+        this.themeMenu = 0;
     }
 }
