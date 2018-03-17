6fdf58b479fa046837afc2374a4e88cf21712897
Kyle Corry <kylecorry31@gmail.com>
2018-03-17 11:04:50 -0400

    Add slim light and dark themes

--- extension.js
+++ extension.js
@@ -28,6 +28,22 @@ function enable() {
         item.setOrnament(Ornament.DOT);
     });
     this.themeMenu.menu.addMenuItem(this.dark, 1);
+
+    this.slim_light = new PopupMenu.PopupMenuItem("Slim Light");
+    this.slim_light.connect('activate', (item, event) => {
+        this.reset_ornament();
+        this.set_theme("Pop-slim");
+        item.setOrnament(Ornament.DOT);
+    });
+    this.themeMenu.menu.addMenuItem(this.slim_light, 2);
+
+    this.slim_dark = new PopupMenu.PopupMenuItem("Slim Dark");
+    this.slim_dark.connect('activate', (item, event) => {
+        this.reset_ornament();
+        this.set_theme("Pop-dark-slim");
+        item.setOrnament(Ornament.DOT);
+    });
+    this.themeMenu.menu.addMenuItem(this.slim_dark, 3);
     
     this.reset_ornament();
     this.light.setOrnament(Ornament.DOT);
@@ -42,6 +58,10 @@ function set_theme(theme) {
         this.themeMenu.label.text = "Light Theme";
     } else if (theme == "Pop-dark") {
         this.themeMenu.label.text = "Dark Theme";
+    } else if (theme == "Pop-slim") {
+        this.themeMenu.label.text = "Slim Light Theme";
+    } else if (theme == "Pop-dark-slim") {
+        this.themeMenu.label.text = "Slim Dark Theme";
     }
 }
 
@@ -57,6 +77,8 @@ function set_user_theme(theme) {
 function reset_ornament() {
     this.light.setOrnament(Ornament.NONE);
     this.dark.setOrnament(Ornament.NONE);
+    this.slim_light.setOrnament(Ornament.NONE);
+    this.slim_dark.setOrnament(Ornament.NONE);
 }
 
 function disable() {
@@ -68,6 +90,16 @@ function disable() {
     if (this.dark) {
         this.dark.destroy();
         this.dark = 0;
+    }
+
+    if (this.slim_dark) {
+        this.slim_dark.destroy();
+        this.slim_dark = 0;
+    }
+
+    if (this.slim_light) {
+        this.slim_light.destroy();
+        this.slim_light = 0;
     }
 
     if (this.themeMenu) {
