74185ae897b264dd1496186db8f8df9bfb8b1ee5
Kyle Corry <kylecorry31@gmail.com>
2018-12-09 14:24:40 -0500

    Remove slim themes for now

--- extension.js
+++ extension.js
@@ -12,7 +12,7 @@ function enable() {
     this.themeMenu = new PopupMenu.PopupSubMenuMenuItem("Theme", true);
     this.mainMenu.addMenuItem(themeMenu, 8);
     this.themeMenu.icon.icon_name = "starred-symbolic";
-    
+
     this.light = new PopupMenu.PopupMenuItem("Light");
     this.light.connect('activate', (item, event) => {
         this.reset_ornament();
@@ -20,7 +20,7 @@ function enable() {
         item.setOrnament(Ornament.DOT);
     });
     this.themeMenu.menu.addMenuItem(this.light, 0);
-    
+
     this.dark = new PopupMenu.PopupMenuItem("Dark");
     this.dark.connect('activate', (item, event) => {
         this.reset_ornament();
@@ -29,22 +29,6 @@ function enable() {
     });
     this.themeMenu.menu.addMenuItem(this.dark, 1);
 
-    this.slim_light = new PopupMenu.PopupMenuItem("Slim Light");
-    this.slim_light.connect('activate', (item, event) => {
-        this.reset_ornament();
-        this.set_theme("Pop-slim");
-        item.setOrnament(Ornament.DOT);
-    });
-    this.themeMenu.menu.addMenuItem(this.slim_light, 2);
-
-    this.slim_dark = new PopupMenu.PopupMenuItem("Slim Dark");
-    this.slim_dark.connect('activate', (item, event) => {
-        this.reset_ornament();
-        this.set_theme("Pop-dark-slim");
-        item.setOrnament(Ornament.DOT);
-    });
-    this.themeMenu.menu.addMenuItem(this.slim_dark, 3);
-    
     this.reset_ornament();
 }
 
@@ -59,10 +43,6 @@ function set_theme_label(theme){
         this.themeMenu.label.text = "Light Theme";
     } else if (theme == "Pop-dark") {
         this.themeMenu.label.text = "Dark Theme";
-    } else if (theme == "Pop-slim") {
-        this.themeMenu.label.text = "Slim Light Theme";
-    } else if (theme == "Pop-dark-slim") {
-        this.themeMenu.label.text = "Slim Dark Theme";
     }
 }
 
@@ -79,8 +59,6 @@ function set_user_theme(theme) {
 function reset_ornament() {
     this.light.setOrnament(Ornament.NONE);
     this.dark.setOrnament(Ornament.NONE);
-    this.slim_light.setOrnament(Ornament.NONE);
-    this.slim_dark.setOrnament(Ornament.NONE);
 }
 
 function disable() {
@@ -88,20 +66,10 @@ function disable() {
         this.light.destroy();
         this.light = 0;
     }
-    
+
     if (this.dark) {
         this.dark.destroy();
         this.dark = 0;
-    }
-
-    if (this.slim_dark) {
-        this.slim_dark.destroy();
-        this.slim_dark = 0;
-    }
-
-    if (this.slim_light) {
-        this.slim_light.destroy();
-        this.slim_light = 0;
     }
 
     if (this.themeMenu) {
